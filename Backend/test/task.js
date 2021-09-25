c380a3583a19809fa8348a9d319f78bdffbc84be
FelipeAveM <37424408+FelipeAveM@users.noreply.github.com>
2021-09-24 22:48:49 -0500

    Unit Tests

---
+++ Backend/test/task.js
@@ -0,0 +1,20 @@
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../server')
+
+//Assertions
+chai.should()
+
+chai.use(chaiHttp)
+
+describe('Task API', () =>{
+
+    /** Test GETs */
+    describe("GET /")
+
+
+
+    /** Test POSTs */
+
+
+})
